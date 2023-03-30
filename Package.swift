e293e6e01bd06684a9e97c1d547ea1d0349e06e5
Sriniva Rao S <srinivass@ideyalabs.com>
2023-03-30 15:11:55 +0530

    New KidozSDK version 1.0.2

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let version = "1.0.1"
+let version = "1.0.2"
 let moduleName = "KidozSDK"
 let checksum = "977b36d2e1a546a233207e5d2f7c172288237a2d8202c05ba9df12eb6236a1db"
 
